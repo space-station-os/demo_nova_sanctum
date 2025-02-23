fd90ae1cc88285b93947e871082b61b15a450048
Siddarth09 <siddarth.dayasagar@gmail.com>
2025-02-23 00:07:22 -0500

    Adding first half of wrs

---
+++ include/demo_nova_sanctum/whc_controller.h
@@ -0,0 +1,27 @@
+#ifndef WHC_CONTROLLER_HPP
+#define WHC_CONTROLLER_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/range.hpp"
+#include "std_msgs/msg/float64.hpp"
+
+class WasteHygieneCompartment : public rclcpp::Node {
+private:
+    rclcpp::Subscription<sensor_msgs::msg::Range>::SharedPtr ultrasound_subscriber_;
+    rclcpp::Publisher<std_msgs::msg::Float64>::SharedPtr water_volume_publisher_;
+
+    bool urination_detected;
+    double urine_volume;         // in liters (approx. 0.3L per urination)
+    double pretreatment_volume;  // in liters (~0.05L per cycle)
+    double flush_volume;         // in liters (~1.0L per cycle)
+    double total_water_volume;   // urine + pretreatment + flush water
+
+public:
+    WasteHygieneCompartment();
+    void urine_callback(const sensor_msgs::msg::Range::SharedPtr msg);
+    void add_pretreatment();
+    void flush();
+    void publish_total_water();
+};
+
+#endif // WHC_CONTROLLER_HPP
